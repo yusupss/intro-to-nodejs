05fe90ac3bee384536c11e562ac2c3f414832203
yusup ss <yusup@softwareseni.com>
2022-02-08 15:31:32 +0700

    CLI Exploration

---
+++ session2/world-cup/index.js
@@ -0,0 +1,58 @@
+const program = require('commander')
+const models = require('./models/manage')
+
+program
+  .version('1.0.0')
+  .description('WORLD CU{ 2018')
+
+
+program
+  .command('refresh')
+  .alias('r')
+  .description('Get newest data from server.')
+  .action(() => {
+    models.refreshData()
+  })
+
+program
+  .command('stadiums')
+  .alias('std')
+  .description('show stadiums')
+  .action(() => {
+    models.getStadiums()
+  })
+
+program
+  .command('tvchannels')
+  .alias('tvc')
+  .description('show TV Channles')
+  .action(() => {
+    models.getTvChannels()
+  })
+
+program
+  .command('teams')
+  .alias('gt')
+  .description('show teams')
+  .action(() => {
+    models.getTeams()
+  })
+
+program
+  .command('group_matches')
+  .alias('gm')
+  .description('show group matches')
+  .action(() => {
+    models.getGroupMatch()
+  })
+
+program
+  .command('match_by_group_name')
+  .alias('mbgn')
+  .description('show group match by name')
+  .action((str, options) => {
+    const groupName = options.args[0]
+    models.getMatchByGroupName(groupName)
+  })
+
+program.parse(process.argv)
---
+++ session2/world-cup/models/data.js
@@ -0,0 +1,68 @@
+const fileSystem = require('fs')
+const db = __dirname+'/../store/db.json'
+const http = require('axios')
+
+const refreshData = () => {
+  http.get('https://raw.githubusercontent.com/lsv/fifa-worldcup-2018/master/data.json')
+      .then((res) => {
+        fileSystem.writeFileSync(db, JSON.stringify(res.data), 'utf8')
+        console.log('')
+        console.info(" score is refreshed")
+      })
+      .catch((err) => {
+        console.info(err.message)
+      })
+}
+
+const getData = () => {
+  return JSON.parse(fileSystem.readFileSync(db, 'utf8'))
+}
+
+const getTeamById = (id) => {
+  const data = getData().teams
+  return data.find(item => item.id == id)
+}
+
+const getTeamName = (id) => {
+  const team = getTeamById(id)
+  if (team) {
+    return team.fifaCode
+  } else {
+    return null
+  }
+}
+
+const getChannelById = (id) => {
+  const data = getData().tvchannels
+  return data.find(item => item.id == id)
+}
+
+const getStadiumById = (id) => {
+  const data = getData().stadiums
+  return data.find(item => item.id == id)
+}
+
+const getStadiumName = (id) => {
+  const stadium = getStadiumById(id)
+  if (stadium){
+    return stadium.name
+  }else{
+    return null
+  }
+}
+
+const getGroupDataByName = (name) => {
+  const groups = Object.entries(getData().groups)
+  return groups.find(([key, value]) => key == name)
+}
+
+module.exports = {
+  refreshData,
+  getData,
+  getTeamById,
+  getTeamName,
+  getChannelById,
+  getStadiumById,
+  getStadiumName,
+  getGroupDataByName
+}
